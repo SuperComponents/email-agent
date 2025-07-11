5148fed6bb5fdc8fb4fbfd5de46aad45a6840a9e
Benjamin Hitov <bhitov@Mac.attlocal.net>
2025-07-11 10:28:37 -0500

    remove agent and agent2

--- agent2/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './types';
-export * from './agent';
-export * from './context/context';
-export * from './context/tools';
-export * from './llm';
-export * from './mocktools';
-export * from './controller';
-export * from './worker-types';
-export { default as runAgentLoop } from './agent-loop';
--- agent2/src/openai.ts
+++
@@ -1,8 +0,0 @@
-import { OpenAI } from 'openai';
-import { OPENAI_API_KEY } from './env.js';
-
-const openai = new OpenAI({ 
-  apiKey: OPENAI_API_KEY,
-});
-
-export default openai;
