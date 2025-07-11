f68cb3787e4a395516b1c1755f2a706bcb863197
Benjamin Hitov <bhitov@Mac.attlocal.net>
2025-07-11 10:09:32 -0500

    add be fe eslint files

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -2,20 +2,26 @@ import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
 
 export default tseslint.config([
-  globalIgnores(['dist', 'drizzle']),
+  globalIgnores(['dist', 'drizzle', '**/*.d.ts']),
   {
     files: ['**/*.{ts,tsx}'],
+    ignores: ['**/*.d.ts'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: {
         ...globals.node,
         NodeJS: true
+      },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
       },
     },
     rules: {
@@ -24,6 +30,17 @@ export default tseslint.config([
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_'
       }],
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-base-to-string': 'warn',
+      '@typescript-eslint/restrict-template-expressions': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
     }
   },
+  {
+    files: ['**/database/**/*.{ts,tsx}'],
+    rules: {
+      '@typescript-eslint/no-empty-object-type': 'off'
+    }
+  },
+  prettier
 ])
